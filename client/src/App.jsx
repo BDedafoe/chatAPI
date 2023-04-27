d0572fcfde137e467535bee6ddd0913365814ea7
BDedafoe <bmd6260@gmail.com>
2023-04-27 12:45:50 -0500

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowswerRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Chat from "@/components/chat";
+import Login from "@/components/login";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <BrowswerRouter>
+          <Routes>
+            <Route path='/chat' element={<Chat />} />
+
+
+          </Routes>
+        </BrowswerRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
